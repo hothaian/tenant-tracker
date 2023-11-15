f7959dbacd8b6cea620037ce3c9ea4bce817c09e
An Ho <41894751+hothaian@users.noreply.github.com>
2023-11-14 23:31:49 -0800

    update header and footer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from './components/Layout/Header';
 import RoomDashboard from './components/RoomDashboard';
 import UtilityForm from './components/Utility/Form';
 import Profile from './components/Profile';
 import SignUp from './components/User/SignUp';
 import Checkout from './components/User/SignUp';
+import SimpleBottomNavigation from './components/Layout/Footer';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
 
 const App = () => {
+
+  const defaultTheme = createTheme();
   return (
+    <ThemeProvider theme={defaultTheme}>
     <div>
-      <Header />
+      {/* <Header /> */}
       <main>
         <RoomDashboard />
         <Checkout/>
         {/* <SignUp /> */}
-        
+        {/* <SimpleBottomNavigation/> */}
         {/* <UtilityForm /> */}
         {/* <Profile /> */}
       </main>
-      <Footer />
+  
     </div>
+    </ThemeProvider>
   );
 };
 
